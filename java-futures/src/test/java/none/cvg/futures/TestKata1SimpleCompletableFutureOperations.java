bda206cab4d8ad3053c23137ef7003bd8a60dae5
Chandra Guntur <chandra.guntur@gmail.com>
2021-04-04 12:38:53 -0400

    Fixes #17: Added back the unused import, for parity with the solution.

--- java-futures/src/test/java/none/cvg/futures/TestKata1SimpleCompletableFutureOperations.java
+++ java-futures/src/test/java/none/cvg/futures/TestKata1SimpleCompletableFutureOperations.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
